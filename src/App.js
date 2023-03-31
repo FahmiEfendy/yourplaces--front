4189eb314b36bf6b6a1fd1a1fa949c0388f624e4
Fahmi <itsfahmiefendy@gmail.com>
2023-03-31 13:50:19 +0700

    feat: add Users and NewPlace component and route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Users />}></Route>
+        <Route path="/places/new" exact element={<NewPlace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,5 @@
+const NewPlace = () => {
+  return <h1>This is New Place!</h1>;
+};
+
+export default NewPlace;
