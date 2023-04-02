cdcf0ad8ea9321d7d6751aaa1b86f276ff0c02b1
Fahmi <itsfahmiefendy@gmail.com>
2023-04-02 20:22:22 +0700

    feat: add feature to retrieve value from Input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -4,7 +4,7 @@ import Input from "../../shared/components/FormElements/Input";
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" />
+      <Input type="text" label="Title" errorText="Please enter a valid title" />
     </form>
   );
 };
