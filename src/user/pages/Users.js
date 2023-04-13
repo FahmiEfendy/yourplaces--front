af0349f508753375cc44995465589e3edf7a8c41
Fahmi <itsfahmiefendy@gmail.com>
2023-04-13 23:11:28 +0700

    feat: integrate get all users API

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,24 +1,56 @@
+import React, { useEffect, useState } from "react";
+
 import UserList from "../components/UserList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Max Schwarz",
-      image:
-        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      places: 3,
-    },
-    {
-      id: "u2",
-      name: "Fahmi Efendy",
-      image:
-        "https://wallpapers-clan.com/wp-content/uploads/2022/07/funny-cat-9.jpg",
-      places: 1,
-    },
-  ];
+  const [error, setError] = useState(null);
+  const [userList, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  return <UserList items={USERS} />;
+  const clearErrorHandler = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:5000/api/users", {
+          method: "GET",
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) throw new Error(responseData.message);
+
+        setUserList(responseData.data);
+        setIsLoading(false);
+      } catch (err) {
+        setError(
+          err.message ||
+            "Something went wrong when get all users, please try again."
+        );
+        setIsLoading(false);
+      }
+    };
+
+    sendRequest();
+  }, []);
+
+  return (
+    <React.Fragment>
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner asOverlay />
+        </div>
+      )}
+      <ErrorModal error={error} onCancel={clearErrorHandler} />
+      <UserList items={userList} />
+    </React.Fragment>
+  );
 };
 
 export default Users;
